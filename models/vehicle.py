673529d08ea4563109acfd09266cf517c7c8f71f
CurcoVain <gg.prof.ef@gmail.com>
2025-04-18 20:18:58 -0300

    metodo post Vehicles con relacion + comentado para guia

--- models/vehicle.py
+++ models/vehicle.py
@@ -9,15 +9,17 @@ class Vehicle(db.Model):
     modelo=db.Column (db.String(20), nullable=False)
     client_id=db.Column(db.Integer,db.ForeignKey('client.id'), nullable=False)
 
-    def __init__(self, client_id, marca, modelo):
-        self.client_id= client_id
+    def __init__(self, marca, modelo, client_id):
         self.marca=marca
         self.modelo=modelo
+        self.client_id= client_id
 
     def serialize(self):
         return{
             'id': self.id,
             'client_id':self.client_id,
             'marca': self.marca,
-            'modelo': self.modelo
+            'modelo': self.modelo,
+            'client_id':self.client.id,
+            'client_name': self.client.name if self.client  else None
         }
